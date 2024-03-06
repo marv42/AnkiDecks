d073a18d5bab9a53471799b5a736a1392940d544
Marvin <marv42+github@gmail.com>
2024-03-06 23:29:01 +0100

    Add abbreviations to Mitarbeiter

--- Mitarbeiter.py
+++ Mitarbeiter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import logging
 import os
 from glob import glob
 from logging import DEBUG
@@ -11,9 +10,9 @@ DECK_NAME_IDS = 'Mitarbeiter: Kürzel'
 PICTURE_PATH = 'dia'
 
 
-def get_name_note(pic_path, name):
+def get_name_note(pic_path, name, abbreviation):
     absolute_path = f"{os.path.abspath(os.getcwd())}/{pic_path}"
-    return get_card(DECK_NAME_NAMES, "Name: ?<br>", name, [absolute_path])
+    return get_card(DECK_NAME_NAMES, "Name: ?<br>", f"{name} ({abbreviation})", [absolute_path])
 
 
 def get_name(pic_path):
@@ -22,11 +21,18 @@ def get_name(pic_path):
     return pic_path[underscore + 1 : dot]
 
 
+def get_abbreviation(pic_path):
+    slash = pic_path.find("/")
+    underscore = pic_path.find("_")
+    return pic_path[slash + 1 : underscore]
+
+
 logging.basicConfig(level=DEBUG)
 invoke('createDeck', deck=DECK_NAME_NAMES)
 pics = glob(f"{PICTURE_PATH}/*.jpg")
 for pic_path in pics:
     name = get_name(pic_path)
-    pic_note = get_name_note(pic_path, name)
+    abbreviation = get_abbreviation(pic_path)
+    pic_note = get_name_note(pic_path, name, abbreviation)
     logging.debug(f"adding {pic_note}")
     invoke('addNote', note=pic_note)
